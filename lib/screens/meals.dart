33f01503981c0801269730a8f42870635165213c
Sujal Acharya <sujalacharya567@gmail.com>
2025-05-24 19:53:41 -0300

    I am back baby

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Sujal'), Row(child: Text('Acharya')));
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("Nothing here"),
+          SizedBox(height: 16),
+          Text(
+            "Try to select another category",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+
+    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
   }
 }
